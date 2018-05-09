ff77d555a7644b2d352daea6646bca038df76c7c
闫肖肖 <yanxiaoxiao@daoxila.com>
2018-05-09 13:46:20 +0800

    clear

---
+++ nuxt.config.1.js
@@ -0,0 +1,48 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'bnhcp',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
+      { hid: 'description', name: 'description', content: 'bnhcp project' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: '/common/common.css' }
+    ]
+  },
+  env: {
+    HOST_URL: process.env.HOST_URL || 'http://localhost:3000'
+  },
+  /*
+  ** Customize the progress-bar colornuxt
+  */
+  loading: { color: '#3B8070' },
+  // or
+  cache: {
+    max: 1000,
+    maxAge: 900000
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    vendor: ['axios', 'babel-polyfill'],
+    /*
+    ** Run ESLINT on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
